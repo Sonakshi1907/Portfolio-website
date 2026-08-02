d9720447df94f97eba5635c82aa3b3f2839f8232
Sonakshi1907 <sonakshisoni225@gmail.com>
2026-08-02 16:27:19 +0530

    Added active navigation

--- script.js
+++ script.js
@@ -17,3 +17,30 @@ scrollTopBtn.addEventListener("click", () => {
     behavior: "smooth",
   });
 });
+// =========================
+// Active Navigation on Scroll
+// =========================
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+  let currentSection = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 150;
+    const sectionHeight = section.offsetHeight;
+
+    if (window.scrollY >= sectionTop) {
+      currentSection = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+
+    if (link.getAttribute("href") === `#${currentSection}`) {
+      link.classList.add("active");
+    }
+  });
+});
